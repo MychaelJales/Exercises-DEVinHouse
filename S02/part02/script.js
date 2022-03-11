5a74ee86cc793928cbb52f99414e654de9ffb432
MychaelJales <mychael_duarte@hotmail.com>
2022-03-11 20:19:38 -0300

    exercicio 9

--- S02/part02/script.js
+++ S02/part02/script.js
@@ -1,8 +1,12 @@
 // alert('Houston, temos um problema!');
 
-const nome = prompt();
+const nome = prompt('Qual seu nome?');
+const idade = Number(prompt('Qual sua idade?'));
+const cidade = prompt('Qual sua cidade?');
 
-console.log(nome);
+console.log(`Seu nome é: ${nome}.`);
+console.log(`Sua idade é: ${idade}.`);
+console.log(`Sua cidade é: ${cidade}.`);
 
 document.querySelector('.text-input').innerHTML = `Seu nome é: ${nome} `;
 
