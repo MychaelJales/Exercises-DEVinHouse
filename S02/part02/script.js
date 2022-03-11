c34e43138fbaaf6168bd7ed6b9df2ccfa7a38c76
MychaelJales <mychael_duarte@hotmail.com>
2022-03-11 20:02:51 -0300

    exercicio 7

--- S02/part02/script.js
+++ S02/part02/script.js
@@ -1,1 +1,11 @@
-alert('Houston, temos um problema!')
+alert('Houston, temos um problema!');
+
+const onClickBtn = () => {
+  const clickBtn = confirm('Deseja realmente clickar ?');
+  if (clickBtn) {
+    document.querySelector('.text-alert').innerHTML = 'Você clicou 😰'
+  }
+};
+
+const btn = document.getElementById('btn');
+btn.addEventListener('click',  onClickBtn);
