86ebf66f3be857865ddb5a8d8a08eda20fc3453d
MychaelJales <mychael_duarte@hotmail.com>
2022-03-15 19:54:49 -0300

    exercicio 4

--- S03/script.js
+++ S03/script.js
@@ -11,22 +11,35 @@ if (querPraticar) {
 } */
 
 // exercício 2
-/* const ePar = () => {
+const ePar = () => {
   const num = Number(document.getElementById('input-num').value);
-  const response = document.getElementById('response');
+  const response = document.getElementById('response1');
   if ((num % 2) === 0 ) {
     response.innerText = 'É par';
   } else {
     response.innerText = 'É ímpar';
   }
-} */
+}
 
 // exercício 3
 const calc = ({ target }) => {
   const num1 = Number(document.getElementById('input-num1').value);
   const num2 = Number(document.getElementById('input-num2').value);
-  const response = document.getElementById('response');
+  const response = document.getElementById('response2');
   const operator = target.value;
   const operation = eval(`${num1} ${operator} ${num2}`);
   response.innerText = `Resposta: ${num1} ${operator} ${num2} = ${operation}`;
+}
+
+// exercício 4
+const calcP = ({ target }) => {
+  const num3 = Number(document.getElementById('input-num3').value);
+  const num4 = Number(document.getElementById('input-num4').value);
+  const response = document.getElementById('response3');
+  const { value : operator, innerText : typeOperation } = target;
+  const result = [num3];
+  for (let i = 0; i < 9; i += 1) {
+    result.push( eval(`${result[i]} ${operator} ${num4}`) );
+  }
+  response.innerText = `Resposta da ${typeOperation}: ${result}`;
 }
