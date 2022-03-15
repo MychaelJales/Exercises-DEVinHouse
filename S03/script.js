5b80dfdb68680cf5502dbf5e47b827eac875c781
MychaelJales <mychael_duarte@hotmail.com>
2022-03-15 19:17:46 -0300

    exercicio 2

--- S03/script.js
+++ S03/script.js
@@ -1,4 +1,4 @@
-const nome = prompt('Qual seu nome?');
+/* const nome = prompt('Qual seu nome?');
 const idade = Number(prompt('Qual sua idade?'));
 
 const querPraticar = confirm('Quer praticar um esporte? ');
@@ -7,4 +7,14 @@ if (querPraticar) {
   alert(`${nome}, de ${idade} anos, quer praticar esportes`);
 } else {
   alert(`${nome}, de ${idade} anos, não quer praticar esportes`);
+} */
+
+const ePar = () => {
+  const num = Number(document.getElementById('input-num').value);
+  const response = document.getElementById('response');
+  if ((num % 2) === 0 ) {
+    response.innerText = 'É par';
+  } else {
+    response.innerText = 'É ímpar';
+  }
 }
