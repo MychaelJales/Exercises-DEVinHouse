2fd505323f844e18e8428e2eef873b35326c8bdc
MychaelJales <mychael_duarte@hotmail.com>
2022-03-15 19:06:00 -0300

    desafio 1

---
+++ S03/script.js
@@ -0,0 +1,10 @@
+const nome = prompt('Qual seu nome?');
+const idade = Number(prompt('Qual sua idade?'));
+
+const querPraticar = confirm('Quer praticar um esporte? ');
+
+if (querPraticar) {
+  alert(`${nome}, de ${idade} anos, quer praticar esportes`);
+} else {
+  alert(`${nome}, de ${idade} anos, não quer praticar esportes`);
+}
