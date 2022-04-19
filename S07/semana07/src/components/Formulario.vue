06bd83cc6662af9620d8d6a7d966dc239de5135c
MychaelJales <mychael_duarte@hotmail.com>
2022-04-19 18:18:00 -0300

    exercicios 9 e 10

--- S07/semana07/src/components/Formulario.vue
+++ S07/semana07/src/components/Formulario.vue
@@ -5,25 +5,39 @@
       <input type="text" class="form-control" v-model="cep" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
     </div>
     <button class="btn btn-dark" type="button" @click="validaCep">Pesquisar CEP</button>
+    <div v-if="end">
+      <p>
+        Endreço do CEP: {{ end.cep }} <br>
+        Rua: {{ end.logradouro }} <br>
+        Bairro: {{ end.bairro }} <br>
+        Número/Complemento: {{ end.complemento }} <br>
+        Cidade: {{ end.localidade }} <br>
+        Estado: {{ end.uf }}
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
+import { ViaCEP } from 'viacep';
+
 export default {
   name: 'FormularioVue',
   data() {
     return {
       cep: '',
+      end: null,
     }
   },
   methods: {
     validaCep() {
       const cep = this.cep.replace(/\D/g, '');
+
       if (cep != '') {
         const regexCep = /^[0-9]{8}$/;
 
         if(regexCep.test(cep)) {
-          console.log('Cep correto');
+          this.buscaCep(cep);
         } else {
           this.cep = '';
           alert("Formato de CEP inválido.");
@@ -32,6 +46,16 @@ export default {
         alert("Digite um CEP válido.");
         this.cep = '';
       }
+    },
+    async buscaCep(cep) {
+      try {
+      const viacep = new ViaCEP()
+      const data = await viacep.cep(cep);
+      this.end = data;
+    } catch (error) {
+      console.log(error)
+    }
+
     },
   }
 }
@@ -44,5 +68,8 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+div p {
+  padding-top: 30px;
 }
 </style>
