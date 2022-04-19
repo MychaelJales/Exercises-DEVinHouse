e3afa8d72a851d4e8e29c3c2f0eced5293f31729
MychaelJales <mychael_duarte@hotmail.com>
2022-04-19 16:50:13 -0300

    exercicio 8

--- S07/semana07/src/components/Formulario.vue
+++ S07/semana07/src/components/Formulario.vue
@@ -2,15 +2,38 @@
   <div class="form">
     <div class="input-group mb-3">
       <span class="input-group-text" id="inputGroup-sizing-default">CEP</span>
-      <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+      <input type="text" class="form-control" v-model="cep" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
     </div>
-    <button class="btn btn-dark" type="button">Pesquisar CEP</button>
+    <button class="btn btn-dark" type="button" @click="validaCep">Pesquisar CEP</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'FormularioVue',
+  data() {
+    return {
+      cep: '',
+    }
+  },
+  methods: {
+    validaCep() {
+      const cep = this.cep.replace(/\D/g, '');
+      if (cep != '') {
+        const regexCep = /^[0-9]{8}$/;
+
+        if(regexCep.test(cep)) {
+          console.log('Cep correto');
+        } else {
+          this.cep = '';
+          alert("Formato de CEP inválido.");
+        }
+      } else {
+        alert("Digite um CEP válido.");
+        this.cep = '';
+      }
+    },
+  }
 }
 </script>
 
