5294038b5676edaf83dcceb78b9ec18305c4feae
MychaelJales <mychael_duarte@hotmail.com>
2022-04-19 16:27:54 -0300

    exercicio 7

--- S07/semana07/src/App.vue
+++ S07/semana07/src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <FormularioVue />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FormularioVue from './components/Formulario.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FormularioVue
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
---
+++ S07/semana07/src/components/Formulario.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="form">
+    <div class="input-group mb-3">
+      <span class="input-group-text" id="inputGroup-sizing-default">CEP</span>
+      <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+    </div>
+    <button class="btn btn-dark" type="button">Pesquisar CEP</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormularioVue',
+}
+</script>
+
+<style scoped>
+.form {
+  width: 80vw;
+  padding-top: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
