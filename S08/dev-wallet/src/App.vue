953d5f425113b053a268c6e55b57716a4431c275
MychaelJales <mychael_duarte@hotmail.com>
2022-05-05 11:58:38 -0300

    exercicio 2

---
+++ S08/dev-wallet/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <my-table :list="list"/>
+
+  </div>
+</template>
+
+<script>
+import MyTable from './components/MyTable.vue';
+
+export default {
+  name: 'App',
+  components: {
+      MyTable,
+  },
+  data() {
+    return {
+      list: [],
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ S08/dev-wallet/src/components/MyTable.vue
@@ -0,0 +1,38 @@
+<template>
+  <h2>Extrado de Movimentações</h2>
+  <div class="my-table">
+    <table v-if="list.length > 0">
+      <thead>
+        <tr>
+          <th>Data</th>
+          <th>Título</th>
+          <th>Tipo</th>
+          <th>Valor</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in list" :key="item.id">
+          <td>{{item.data}}</td>
+          <td>{{item.titulo}}</td>
+          <td>{{item.tipo}}</td>
+          <td>{{item.valor}}</td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+    <p v-else>Não existem dados para serem exibidos</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyTable',
+  props: {
+    list: Array,
+  }
+}
+</script>
+
+<style>
+</style>
