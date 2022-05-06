a9861aa8c568e80237b400974d6f29350ad6e02d
MychaelJales <mychael_duarte@hotmail.com>
2022-05-05 22:14:28 -0300

    exercicios 05

--- S09/primeiros-exercicios/src/App.vue
+++ S09/primeiros-exercicios/src/App.vue
@@ -22,6 +22,16 @@
       <label :style="{ color: ex04 }" for="ex04">Cor</label>
       <input id="ex04" type="text" v-model="ex04">
     </div>
+    <hr>
+    <div>
+      <p>Exercício 05</p>
+      <label v-if="ex05 === 'black'" :style="{ color: ex05 }" v-text="'Label Preto'"></label>
+      <label v-else-if="ex05 === 'red'" :style="{ color: ex05 }" v-text="'Label Vermelho'"></label>
+      <label v-else :style="{ color: ex05 }" v-text="'Outra cor'"></label>
+      <button @click="ex05 = 'black'">Mostrar label Preto</button>
+      <button @click="ex05 = 'red'">Mostrar label <span :style="{ color: 'red' }">Vermelho</span></button>
+      <button @click="ex05 = 'blue'">Mostrar <span :style="{ color: 'blue' }">Outra cor</span></button>
+    </div>
   </div>
 </template>
 
@@ -33,6 +43,7 @@ export default {
       ex01: '',
       ex02: 'Old',
       ex04: '',
+      ex05: 'Label Preto',
       html: '<h6>Banner</h6><img src="https://softauthor.com/wp-content/uploads/2020/04/create-a-banner-hero-unit-in-css-1-1024x365.png" style="width:300px"/>'
     }
   }
