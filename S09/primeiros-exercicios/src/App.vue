cc39e0d70fcfcef9ec1201eb41184c888f9fa8a9
MychaelJales <mychael_duarte@hotmail.com>
2022-05-05 21:29:07 -0300

    exercicios 01

---
+++ S09/primeiros-exercicios/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div>
+      <p>Exercício 01</p>
+      <h1 v-text="ex01"></h1>
+      <input type="text" v-model="ex01">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      ex01: '',
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
